1ff828fd7d96b88ae313d6bff23d9e2cb24e61a2
Aakanksha <aakanskshakamti@gmail.com>
2024-11-19 17:52:06 +0530

    new files added

---
+++ src/main/java/com/add/demo/seproject/App.java
@@ -0,0 +1,13 @@
+package com.add.demo.seproject;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
